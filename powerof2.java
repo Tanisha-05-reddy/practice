03c0e2a094d34b5e05c581d48b1eaba111c9538e
Tanisha-05-reddy <tanisha.potham@gmail.com>
2025-11-07 17:52:01 +0530

    Create powerof2.java

---
+++ powerof2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+		// your code goes here
+		int a = 2;
+		while (a<=128)
+		{
+		    System.out.println(a);
+		    a*=2;
+		}
+
+	}
+}
